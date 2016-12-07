a2ac429764439e95e3ffb551f44f28bad0187a2f
Stjarnan <stiernstrom_3@hotmail.com>
2016-12-07 01:24:27 +0100

    Added basic draw functionality

---
+++ js/canvas.js
@@ -0,0 +1,52 @@
+var canvas = document.getElementById('canvas-draw');
+var ctx = canvas.getContext('2d');
+var draw = false;
+var positionX = new Array();
+var positionY = new Array();
+var mouseDrag = new Array();
+var rect = canvas.getBoundingClientRect();
+var canvasDiv = document.getElementById('canvas');
+canvas.width = canvasDiv.offsetWidth;
+canvas.height = canvasDiv.offsetHeight;
+function drawPosition(x, y, movement) {
+    positionX.push(x);
+    positionY.push(y);
+    mouseDrag.push(movement);
+}
+function redraw() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, ctx.width, ctx.height);
+    ctx.strokeStyle = "black";
+    ctx.lineJoin = "round";
+    ctx.lineWidth = 2;
+    for (var i = 0; i < positionX.length; i++) {
+        ctx.beginPath();
+        if (mouseDrag[i] && i) {
+            ctx.moveTo(positionX[i - 1], positionY[i - 1]);
+        }
+        else {
+            ctx.moveTo(positionX[i] - 1, positionY[i]);
+        }
+        ctx.lineTo(positionX[i], positionY[i]);
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+// Event listeners
+canvas.addEventListener("mousedown", function (e) {
+    draw = true;
+    drawPosition(e.pageX - rect.left, e.pageY - rect.top);
+    redraw();
+});
+canvas.addEventListener("mousemove", function (e) {
+    if (draw) {
+        drawPosition(e.pageX - rect.left, e.pageY - rect.top, true);
+        redraw();
+    }
+});
+canvas.addEventListener("mouseup", function (e) {
+    draw = false;
+});
+canvas.addEventListener("mouseleave", function (e) {
+    draw = false;
+});
--- js/canvas.ts
+++ js/canvas.ts
@@ -1,3 +1,71 @@
 
-let canvas = document.getElementsByTagName('canvas');
+let canvas : any = document.getElementById('canvas-draw');
+let ctx = canvas.getContext('2d');
+let draw = false;
+let positionX = new Array();
+let positionY = new Array();
+let mouseDrag = new Array();
+let rect = canvas.getBoundingClientRect();
+let canvasDiv : any = document.getElementById('canvas'); 
 
+canvas.width = canvasDiv.offsetWidth;
+canvas.height = canvasDiv.offsetHeight;
+
+
+function drawPosition(x, y, movement) {
+    positionX.push(x);
+    positionY.push(y);
+    mouseDrag.push(movement);
+}
+
+
+function redraw () {
+    // Clear the canvas
+    ctx.clearRect(0, 0, ctx.width, ctx.height);
+
+
+    ctx.strokeStyle = "black";
+    ctx.lineJoin = "round";
+    ctx.lineWidth = 2;
+
+    for( var i = 0; i < positionX.length; i++) {
+        ctx.beginPath();
+
+        if(mouseDrag[i] && i) {
+            ctx.moveTo(positionX[i-1], positionY[i-1]);
+        } else {
+            ctx.moveTo(positionX[i]-1, positionY[i]);
+        }
+
+        ctx.lineTo(positionX[i], positionY[i]);
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+}
+
+
+
+ // Event listeners
+        canvas.addEventListener("mousedown", e => {
+
+            draw = true;
+            drawPosition(e.pageX  - rect.left, e.pageY - rect.top);
+            redraw();
+        }); 
+
+        canvas.addEventListener("mousemove", e => {
+            
+            if (draw) {
+                drawPosition(e.pageX - rect.left, e.pageY - rect.top, true);
+                redraw();
+            }
+        });
+
+        canvas.addEventListener("mouseup", e => {
+            draw = false;
+        });
+
+        canvas.addEventListener("mouseleave", e => {
+            draw = false;
+        });
